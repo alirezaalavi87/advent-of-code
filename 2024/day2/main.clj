b9bc1bf163361720899fd2431d1f178e3774a1d0
Alireza Alavi <alavialireza@protonmail.com>
2025-05-30 22:02:00 +0330

    refactor(day2-part1): refactor clojure solution to more modular functional style

--- 2024/day2/main.clj
+++ 2024/day2/main.clj
@@ -16,19 +16,39 @@
        (map #(str/split % #" "))
        (map #(map parse-long %))))
 
-;; check each level in report:
-;;1-is either ascending or all descending
-;;2-no diff between two adjacent elements are bigger than 3
+;;;;;;;;;;;;;;;;;
+;     PART1     ;
+;;;;;;;;;;;;;;;;
+
+;; If all records are either increasing or decreasing
+(defn monotonic-diffs? [diffs]
+  (or (every? pos? diffs) (every? neg? diffs)))
+
+;; If all diffs are within the specified bounds
+(defn diffs-within-bounds? [diffs]
+  (every? #(and (<= (abs %) 3) (>= (abs %) 1)) diffs))
+
+;; Check if report is safe
+;; 1. The levels are either all increasing or all decreasing.
+;; 2. Any two adjacent levels differ by at least one and at most three.
 (defn safe-report? [report]
   ; get the diff of 1st element with 2nd, 3rd with 4th and so on...
   (let [diffs (map #(- %2 %1) report (rest report))]
-    ; all diffs are positive AND the abs is smaller than 3
-    ;OR all diffs are negative AND the abs is smaller than 3
-    (or (and (every? pos? diffs) (every? #(<= (abs %) 3) diffs))
-        (and (every? neg? diffs) (every? #(<= (abs %) 3) diffs)))))
+    (and (monotonic-diffs? diffs) (diffs-within-bounds? diffs))))
 
 ;; count the safe reports
 (defn count-safe-reports [reports]
   (count (filter safe-report? reports)))
+
+;;;;;;;;;;;;;;;;;
+;     PART2     ;
+;;;;;;;;;;;;;;;;
+;; Part2 is the same as part1 but now we have tolerance for one bad level.
+
+;; METHOD:
+;; Check if report is safe like PART1
+;; If report is unsafe, start removing each element. if it becomes safe by removing an element,
+;;The report is safe. if not, the report is not safe
+; (defn safe-report-with-removal? [report])
 
 (println (count-safe-reports input-reports))
