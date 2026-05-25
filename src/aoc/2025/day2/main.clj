2f92663a5b7b5a6401b178ec4032a87e0b6a9e74
Alireza Alavi <alavialireza@protonmail.com>
2026-05-25 19:45:11 +0330

    feat(d2): optimize part2, fix typos and writing.

--- src/aoc/2025/day2/main.clj
+++ src/aoc/2025/day2/main.clj
@@ -174,7 +174,7 @@
 ; (c/quick-bench (map repeated-twice? random-numbers))
 ; (c/quick-bench (map repeated-twice?-opt random-numbers))
 ; ```
-; 13.73ms vs. 11.32ms mean execution time after optimizations. That's ~18%.
+; 13.73ms vs. 11.32ms mean execution time after optimizations. That's \~18%.
 
 ; To optimize `num->digits?`
 ; - make sure `num->digits?` returns a vector instead of a list, with `mapv` instead of `map`,
@@ -213,7 +213,7 @@
 ; ; (out)    Execution time upper quantile : 9.872587 sec (97.5%)
 ; ; (out)                    Overhead used : 6.769222 ns
 ; ```
-; Boy, Oh boy! We got ~87.3% increase in performance after our optimizations!\
+; Boy, Oh boy! We got \~87.3% increase in performance after our optimizations!\
 ; These are still cookie-cutter optimizations, tough. I think we can get much further
 ; with more in depth optimizations.
 
@@ -262,7 +262,7 @@
 ; ; (out) Evaluation count : 6 in 6 samples of 1 calls.
 ; ; (out)              Execution time mean : 572.113409 ms
 ; ```
-; This make our total optimizations **~17x faster** than our initial solution.
+; This make our total optimizations **\~17x faster** than our initial solution.
 
 ; ## Part 2
 
@@ -288,9 +288,9 @@
 ; n=3 => [112 112 112]\
 ; groups are equal ✅
 
-; ~The groups should be strings of numbers so a gruop like "001" can still exist.~\
+; ~The groups should be strings of numbers so a group like "001" can still exist.~\
 ; The groups should be sequence of numbers `(\1 \1 \2)` because there is no need to create
-; strings from them which is a very costly process. In profiling, `str/join` took ~75% of our computation time.
+; strings from them which is a very costly process. In profiling, `str/join` took \~75% of our computation time.
 
 ; ### Implementation
 
@@ -347,27 +347,163 @@
 ; Check if the answer for test input is correct:
 (assert (= 4174379265 (part-2 test-input)))
 
-#_(c/quick-bench (part-2 (parse-input "src/aoc/2025/day2/input.txt")))
-
 ; ### Optimization
 
 ; We have already applied the optimizations from part 1.\
-; Our slution is quite slow as of now. (~15s execution time mean for full input).
+; Our slution is quite slow as of now. (\~15s execution time mean for full input).
 ;
 ; Let's profile `part-2` with the full input.
 (clerk/image "src/aoc/2025/day2/assets/profp2-1.png")
 (clerk/image "src/aoc/2025/day2/assets/profp2-1-zoom.png")
 
-; To my understanding of the profiling results, and my current knowledge of Clojure and optimizing it,
-; I can't think of a way to make this algorithm faster. The type hints also make no difference.\
-; I think memoization can help with this problem, in places where recursion is happening.
-;
-; The complexity of `num-split-n` as it is with two nested `loop`s is `n^2`. How can we make it better?
-;
-; I think since the algorithm itself is a bruteforce algorithm and can't be very performant, Maybe create
-; a better algorithm?
+;; The main culprit is `num-split-n` within `repeated-digits?`.
+;; Turns out, clojure has a function `partition-all` which does exactly what we want!\
+;; Clojure's stdlib never ceases to amaze me! it has such amazing and useful functions!\
+;; Also, we dont *need* the number split to be a vector, it can be a list since we just
+;; want to check the equality of splits.
 
-; ## Observations and lessions
+;; So we will optimize and rewrite `repeated-digits?`
+;; - Gets rid of num-split-n entirely
+;; - `partition-all` returns group seqs lazily; every? compares groups without expanding args.
+;; - Avoids apply by doing a manual loop
+;; - compute `digits`, `len`, `max-len` only once
+;; - only check `n` that devides the length of the number
+
+(defn repeated-digits?-opt
+  "Return true if number is made of repeated digits."
+  [^long number]
+  (let [digits (num->digits number)
+        len (count digits)
+        max-n (quot len 2)]
+    (loop [n 1]
+      (cond
+        ;; If our period is larger than half of length, then number cannot be
+        ;; repeated in periods of n.
+        (> n max-n) false
+        (not (zero? (mod len n))) (recur (inc n)) ; skip non-divisors
+        :else
+        (let [groups (partition-all n digits)
+              firstg (first groups)]
+          (if (and firstg (every? #(= firstg %) (rest groups)))
+            true
+            (recur (inc n))))))))
+
+;; Let's benchmark them
+
+(with-out-str
+  (c/quick-bench (with-redefs-fn {#'num->digits num->digits-opt3}
+                   #(repeated-digits? 123456789012345678))))
+(with-out-str
+  (c/quick-bench (with-redefs-fn {#'num->digits num->digits-opt3}
+                   #(repeated-digits?-opt 123456789012345678))))
+
+;; This is \~57% faster
+
+; ```clojure
+; (c/quick-bench (part-2 (parse-input "src/aoc/2025/day2/input.txt")))
+; ; =>
+; ; (out) Evaluation count : 6 in 6 samples of 1 calls.
+; ; (out)              Execution time mean : 14.848484 sec
+; ; (out)     Execution time std-deviation : 81.032469 ms
+; (c/quick-bench (with-redefs-fn
+;                  {#'repeated-digits? repeated-digits?-opt
+;                   #'num->digits num->digits-opt3}
+;                  #(part-2 (parse-input "src/aoc/2025/day2/input.txt"))))
+; ; =>
+; ; (out) Evaluation count : 6 in 6 samples of 1 calls.
+; ; (out)              Execution time mean : 6.660334 sec
+; ; (out)     Execution time std-deviation : 164.315838 ms
+; ```
+
+;; Our total execution time of part-2 is down by \~55%.
+
+;; For further optimization, I can't think of anything since my knowledge on Clojure is
+;; pretty limited at the moment.\
+;; So I ask ChatGPT5-mini for help.\
+;; Key changes:
+;; - convert digits once into a primitive int-array for fast indexed access (avoids boxed collections).
+;; - avoid allocations from partition/all/seq; compare in-place by index.
+;; - skip n values that do not divide length.
+;; - use unchecked integer ops and simple loops to minimize boxing.
+
+;; So I dug deep to understand why and how these changes benefit our performance.
+;; - convert digits once into a primitive int-array for fast indexed access
+;;    with `digits->int-array` (avoids boxed collections).
+;; - Instead of `apply`ing `=` to our number splits, check equality of digits
+;;    in-place by index (in `repeat-pattern?`). This avoids
+
+;; In `repeated-digits?-opt` we used `partition-all` to split the sequence of our
+;; number's digits into groups of n, and then using `every?` to see if all splits
+;; are equal.\
+;; Here's what this does:\
+;; `partition-all` creates seq of coll, then loads the first n items of the seq
+;; into memory at once with `doall`. It then recurses with the `nthrest` of the
+;; collection and keeps adding the results together and then returns them in a `lazy-seq`.\
+;; This creates many intermediary seqs and repeated traversals
+
+;; In `repeated-digits?-opt2` we convert our digits into a primitive int-array
+;; for fast indexed access (`digits->int-array`). This avoids boxed collections.\
+;; Then, we check if our array of ints is repeated with `repeated-pattern?` for
+;; groups of n.\
+;; The difference here from previous implementation is that we will check equality of
+;; ints in-place by their index, with unchecked comparisons. This enables unboxed primitive
+;; operations for us instead of creating boxed values and seqs and traversing them.\
+;; If our previous conditions did not hold, we will recurse `repeated-digits?-opt2`
+;; with bigger n until `max-n`.
+
+(defn- digits->int-array
+  "Convert list of digits to array of int"
+  [digits]
+  (let [len (count digits)
+        arr (int-array len)]
+    (loop [i 0 d digits]
+      (when (< i len)
+        (aset-int arr i (int (first d)))
+        (recur (inc i) (rest d))))
+    arr))
+
+(defn- repeat-pattern?
+  "Return true if arr repeats with period n."
+  [^ints arr ^long len ^long n]
+  (loop [i n]
+    (cond
+      (>= i len) true
+      (== (aget arr i) (aget arr (mod i n))) (recur (inc i))
+      :else false)))
+
+#_(repeat-pattern? (digits->int-array (num->digits-opt3 12341234)) 8 8)
+
+;; Using a manual `loop` instead of `apply` for checking the equality,
+(defn repeated-digits?-opt2
+  "True if number is made of repeated digits. Single-threaded, allocation-minimal."
+  [^long number]
+  (let [digits (num->digits number)
+        len (count digits)
+        max-n (quot len 2)
+        arr (digits->int-array digits)]
+    (loop [n 1]
+      (cond
+        (> n max-n) false
+        (not (zero? (mod len n))) (recur (inc n))
+        (repeat-pattern? arr len n) true
+        :else (recur (inc n))))))
+
+;; Note it doesn't matter if `num->digits` returns digits as char representation or
+;; ints, because we just want to compare. The actual value doesn't matter.
+; ```clojure
+; (c/quick-bench (with-redefs-fn
+;                  {#'repeated-digits? repeated-digits?-opt2
+;                   #'num->digits num->digits-opt3}
+;                  #(part-2 (parse-input "src/aoc/2025/day2/input.txt"))))
+; ; =>
+; ; (out) Evaluation count : 6 in 6 samples of 1 calls.
+; ; (out)              Execution time mean : 2.057931 sec
+; ; (out)     Execution time std-deviation : 14.719844 ms
+; ```
+;; This is \~87% faster than our first implementation.\
+;; Or in more click-baity words, **\~ x7.5 times faster!**
+
+; ## Observations and lessons
 
 ; - Benchmarking results aren't consistent between different days. Same function with same
 ;   inputs can give varying results in terms of absolutes (Execution mean time for example).
